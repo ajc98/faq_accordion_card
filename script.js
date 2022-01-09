6435ae9e4361e9704fdaa4cc55cee6b7204c8d3d
ajc98 <aljica920@gmail.com>
2022-01-09 17:45:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ console.log('Hello')
 
 
 const showAnswer = (question_id) => {
-    for(let i = 1; i <= 5; i++){
-        document.getElementById('answer'+i.toString()).style.display = 'none';
-    }
     var question_number = question_id.slice(-1);
     var answer_number = 'answer'+question_number
     console.log('The answer number is: '+answer_number)
@@ -13,6 +10,9 @@ const showAnswer = (question_id) => {
     if(element_shown.style.display == 'block'){
         element_shown.style.display = 'none';
     }else{
+        for(let i = 1; i <= 5; i++){
+            document.getElementById('answer'+i.toString()).style.display = 'none';
+        }
         element_shown.style.display = 'block';
     }
     //document.getElementById(answer_number).style.display = 'block'
